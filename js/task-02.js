b09c0bff29d230333e4e95197a80f3fb3d4e03b2
YuriiRodenko <jury.rodenko@gmail.com>
2023-04-24 23:14:37 +0300

    add hw

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsEl = document.querySelector("#ingredients");
+
+console.log(ingredientsEl);
+console.log(ingredients);
+
+const listRefs = ingredients.map((ingredient) => {
+  const listRef = document.createElement("li");
+  listRef.textContent = ingredient;
+  listRef.classList.add("item");
+  return listRef;
+});
+
+ingredientsEl.append(...listRefs);
